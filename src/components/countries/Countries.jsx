a1600421d186942e95146814e3e9a2a1680c0886
Essien Nathaniel <thaniessy@gmail.com>
2023-01-05 13:24:57 +0000

    All countries displaying properly

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './countries.module.css'
+
+const Countries = (props) => {
+  if (!props.countryList) {
+    return
+  }
+  const countryArray = props.countryList
+  const cards = countryArray.map((country) => {
+    return <div className={styles.container} key={country.name.official}>
+      <div className={styles.image}>
+        <img src={country.flags.png} alt={country.name.official} />
+      </div>
+     <div className={styles.text}>
+      <h3>{country.name.official}</h3>
+      <p><strong>Population:</strong> {country.population}</p>
+      <p><strong>Region:</strong> {country.region}</p>
+      <p><strong>Capital:</strong> {country.capital}</p>
+     </div>
+    </div>
+  })
+
+  // console.log(countryArray)
+ 
+
+  return (
+    <section>
+      {cards}
+    </section>
+  )
+}
+
+export default Countries
+
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,61 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import inputStyle from './input.module.css'
 import { BiSearch } from 'react-icons/bi'
+import Countries from '../countries/Countries'
 
 const Input = () => {
   const [searchTerm, setSearchTerm] = useState("")
-  const [ filter, setFilter ] = useState('')
-  const [term, setTerm] = useState('name')
-  const [fetchMe, setFetchMe] = useState(`https://restcountries.com/v3.1/${term}`)
+  const [allCountries, setAllCountries] = useState()
+  const [filter, setFilter] = useState('')
+  const [term, setTerm] = useState('all')
 
-function handleChange(e){
-  setSearchTerm(e.target.value)
-  console.log(searchTerm)
-}
 
-  
+  function handleChange(e) {
+    setSearchTerm(e.target.value)
+    // console.log(searchTerm)
+  }
+
+
   function getData() {
-   if(searchTerm === ''){
-     setTerm('all')     
-   }else{
-     setTerm('name') 
-       &&  
-       setFetchMe(`https://restcountries.com/v3.1/${term}/${searchTerm}`)
-   }
-      fetch(fetchMe)
+    if (searchTerm !== '') {
+      setTerm('name')
+    }
+    fetch(`https://restcountries.com/v3.1/${term}/${searchTerm}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        const countries = data
+        // console.log(data)
+        setAllCountries(countries)
       })
       .catch(err => console.log(err))
-      }
+  }
 
-  
+
   useEffect(() => {
     const timedData = setTimeout(getData(), 5000)
-    
-    return ()=>{
+
+    return () => {
       clearTimeout(timedData)
     }
-  }, [])
+  }, [searchTerm])
 
 
   function formSubmitHandler(event) {
     event.preventDefault()
-     }
+    console.log(allCountries)
+    setSearchTerm('')
+  }
 
   return (
+    <>
     <form className={inputStyle.form} onSubmit={formSubmitHandler} >
       <div className={inputStyle.inputDiv}>
-       <input 
-         value={searchTerm}
-         />
+        <input
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder='Please search here'
+        />
         <div className={inputStyle.search}> <BiSearch /></div>
       </div>
       <select
         className={inputStyle.select}
         value={filter}
-        onChange={(e)=>{setFilter(e.target.value)}}
+        onChange={(e) => { setFilter(e.target.value) }}
       >
         <option value=''>Filter by region</option>
         <option value='Africa' >Africa</option>
@@ -64,6 +69,10 @@ function handleChange(e){
         <option value='Europe' >Europe</option>
       </select>
     </form>
+      <Countries 
+        countryList={allCountries}
+        />
+      </>
 
   )
 }
